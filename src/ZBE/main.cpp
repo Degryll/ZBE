e8383de6b9bdb920bd794093ab20759432ae0d9a
Degryll <x@x.com>
2014-02-26 22:11:07 +0100

    -SDL2 added -saving some local work

--- src/ZBE/main.cpp
+++ src/ZBE/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <cstdio>
+
+#include <SDL2/SDL.h>
 
 #include "ZBE/test.h"
 
-int main() {
+int main(int argc, char *argv[]) {
     ClaseTest t(4);
     t.setValue(42);
     std::cout << "Hello world! " << t.getValue() << std::endl;
+    getchar();
     return 0;
 }
