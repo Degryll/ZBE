00671a2f1ab97de9a21c0e8100e7c48a2650ad4f
Degryll <degryll@gmail.com>
2016-08-21 19:34:41 +0200

    - fixed EventStore test

--- src/ZBETest/core/events/EventStoreTest.cpp
+++ src/ZBETest/core/events/EventStoreTest.cpp
@@ -6,28 +6,26 @@
 #include "ZBE/core/events/EventStore.h"
 
 TEST(EventStore, EventStore) {
-//  Event is abstract
-//  zbe::Event e0(0,104);
-//  zbe::Event e1(1,100);
-//  zbe::Event e2(2,100);
-//  zbe::Event e3(3,101);
-//  zbe::Event e4(4,104);
-//
-//  zbe::EventStore &store = zbe::EventStore::getInstance();
-//  std::forward_list<zbe::Event*> eventlist = store.getEvents();
-//
-//  EXPECT_EQ(true, eventlist.empty()) << "Must be empty at init";
-//
-//  store.storeEvent(&e0);
-//  store.storeEvent(&e1);
-//  store.storeEvent(&e2);
-//  store.storeEvent(&e3);
-//
-//  eventlist = store.getEvents();
-//
-//  auto it = eventlist.begin();
-//
-//  EXPECT_EQ(2u,(*it++)->getId()) << "Must find e2 first";
-//  EXPECT_EQ(1u,(*it++)->getId()) << "Must find e1 next";
-//  EXPECT_EQ(eventlist.end(),it) << "Must found no more";
+  zbe::TimeEvent e0(0,104, 0);
+  zbe::TimeEvent e1(1,100, 1);
+  zbe::TimeEvent e2(2,100, 2);
+  zbe::TimeEvent e3(3,101, 3);
+
+  zbe::EventStore &store = zbe::EventStore::getInstance();
+  std::forward_list<zbe::Event*> eventlist = store.getEvents();
+
+  EXPECT_EQ(true, eventlist.empty()) << "Must be empty at init";
+
+  store.storeEvent(&e0);
+  store.storeEvent(&e1);
+  store.storeEvent(&e2);
+  store.storeEvent(&e3);
+
+  eventlist = store.getEvents();
+
+  auto it = eventlist.begin();
+
+  EXPECT_EQ(2u,(*it++)->getId()) << "Must find e2 first";
+  EXPECT_EQ(1u,(*it++)->getId()) << "Must find e1 next";
+  EXPECT_EQ(eventlist.end(),it) << "Must found no more";
 }
