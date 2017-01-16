196db7410144802a74f35f21f38cef76589f6984
Degryll <degryll@gmail.com>
2017-01-16 21:56:37 +0100

    Setup #59

---
+++ include/ZBE/core/tools/containers/ListofLists.h
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2011 Batis Degryll Ludo
+ * @file ListofLists.h
+ * @since 2016/03/31
+ * @date 2016/08/04
+ * @author Degryll
+ * @brief A class that encapsulates many lists in one.
+ */
+
+#ifndef CORE_TOOLS_CONTAINERS_LISTOFLISTS_H_
+#define CORE_TOOLS_CONTAINERS_LISTOFLISTS_H_
+
+namespace zbe {
+
+
+}  // namespace zbe
+
+#endif  // CORE_TOOLS_CONTAINERS_LISTOFLISTS_H_
