58e462a918766cf27f63f13f75bafc553af873dc
RubenBatis <RubenBatis@gmail.com>
2017-04-05 23:12:14 +0200

    Batismain does cool things with it`s TtpHandler

---
+++ src/ZBE_main/include/batis/entities/GameBoard.h
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2012 Batis Degryll Ludo
+ * @file GameBoard.h
+ * @since 2017-01-25
+ * @date 2017-01-25
+ * @author Degryll Batis Ludo
+ * @brief The gameboard map for an arkanoid.
+ */
+
+#ifndef ZBE_MAIN_GAME_GAMEBOARD
+#define ZBE_MAIN_GAME_GAMEBOARD
+
+#include <cstdint>
+#include <memory>
+
+#include "ZBE/core/tools/math/objects.h"
+#include "ZBE/core/entities/avatars/Collisioner.h"
+#include "ZBE/core/entities/avatars/implementations/SimpleCollisioner.h"
+#include "ZBE/core/entities/AvatarEntity.h"
+#include "ZBE/reactobjects/VoidReactObject.h"
+
+#include "game/GameReactor.h"
+
+namespace batis {
+
+struct Board {
+  int32_t margin;
+  int32_t space;
+  int32_t brickWidth;
+  int32_t brickHeight;
+  int32_t cols;
+  int32_t rows;
+  int32_t brickAreaWidth;
+  int32_t brickAreaHeight;
+};
+
+class GameBoard : public zbe::AvatarEntityFixed<zbe::Collisioner<game::GameReactor> > {
+public:
+  Board board;
+  GameBoard(double x, double y, double width, double height, uint64_t actuatorsList, Board board) :
+    zbe::AvatarEntityFixed<zbe::Collisioner<game::GameReactor> >(new zbe::SimpleCollisioner<game::GameReactor>(std::make_shared<zbe::StaticLimiterAABB2D<game::GameReactor> >(zbe::AABB2D({x, y}, {width, height} )),
+         std::make_shared<zbe::VoidReactObject<game::GameReactor> >(),
+         actuatorsList)), board(board){}
+
+};
+
+} // namespace batis
+
+#endif //ZBE_MAIN_GAME_GAMEBOARD
--- src/ZBE_main/include/batis/events/handlers/TtpHandler.h
+++ src/ZBE_main/include/batis/events/handlers/TtpHandler.h
@@ -8,41 +8,49 @@
 #include "ZBE/core/entities/AvatarEntity.h"
 #include "ZBE/entities/avatars/Positionable.h"
 #include "ZBE/core/events/generators/TimeEventGenerator.h"
+#include "batis/entities/GameBoard.h"
 
-namespace game {
+
+namespace batis {
 
 class TtpHandler : public zbe::TimeHandler {
 	public:
 	  TtpHandler(const TtpHandler&) = delete;
 	  void operator=(const TtpHandler&) = delete;
 
-  	TtpHandler(zbe::AvatarEntity<zbe::Positionable<2> >* entity, zbe::TimeEventGenerator &teg) : e(entity), teg(teg) {}
+  	TtpHandler(zbe::AvatarEntity<zbe::Positionable<2> >* entity, zbe::TimeEventGenerator &teg, batis::GameBoard &board) : e(entity), teg(teg), board(board) {}
 
   	void run(uint64_t time) {
       zbe::Positionable<2>* avatar;
       e->assignAvatar(&avatar);
 
       zbe::Point<2> position = avatar->getPosition();
+      position[0] = (position[0]-board.board.margin)/(board.board.brickWidth + board.board.space);
+      position[1] = (position[1]-board.board.margin)/(board.board.brickHeight + board.board.space);
 
-      if (position[0] >= 300){
-      	position[0] = 100;
-        position[1] += 50;
-	      if (position[1] > 300) {
-      		position[1] = 100;
+      if (position[0] >= board.board.cols-1){
+      	position[0] = 0;
+        position[1] += 1;
+	      if (position[1] > board.board.rows-1) {
+      		position[1] = 0;
 	      }
       } else {
-        position[0] += 50;
+        position[0] += 1;
       }
+      position[0] = board.board.margin + (position[0] * (board.board.brickWidth + board.board.space));
+      position[1] = board.board.margin + (position[1] * (board.board.brickHeight + board.board.space));
+
       avatar->setPosition(position);
-      std::shared_ptr<zbe::TimeHandler> t = std::make_shared<TtpHandler>(e, teg);
-      teg.addTimer(t,time + (zbe::SECOND*2));
+      std::shared_ptr<zbe::TimeHandler> t = std::make_shared<batis::TtpHandler>(e, teg, board);
+      teg.addTimer(t,time + (zbe::SECOND));
   	}
 
 	private:
   	zbe::AvatarEntity<zbe::Positionable<2> >* e;
   	zbe::TimeEventGenerator &teg;
+  	batis::GameBoard &board;
 };
 
-} //namespace game
+} //namespace batis
 
 #endif //ZBE_MAIN_GAME_EVENTS_HANDLERS_TTPHANDLER
