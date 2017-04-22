59cca8224e32eebd8786028a2f3eaf8f8035232d
Ludovicio <isra.bautista@gmail.com>
2017-04-22 15:33:01 +0200

    issue #74 Inital math tools added.

--- include/ZBE/core/tools/math/math.h
+++ include/ZBE/core/tools/math/math.h
@@ -16,6 +16,8 @@
 #include <utility>
 #include <initializer_list>
 
+#include "ZBE/core/tools/math/Point.h"
+
 namespace zbe {
 
 /** \brief The value of PI as 3.14159265358979323846.
@@ -31,33 +33,6 @@ static const double TORADIANS = PI/180.0;
     \sa TORADIANS
  */
 static const double TODEGREE = 180.0/PI;
-
-///** \brief This constant represent the amount of bits that will be used as precision digits.
-// * I.E. :
-// *  uint64_t v1;  // velocity in pixel / sec.
-// *  uint64_t v2;  // velocity in pixel / nanosec for collision purpouses.
-// *  ...
-// *  v2 = v1 << PRECISION_DIGITS;
-// *  ...
-// *  v1 = v2 >> PRECISION_DIGITS;
-// */
-//static const int PRECISION_DIGITS = 16;
-//
-///** \brief This constant represent the minimal value that will keep its value after be rounded.
-// */
-//static const int64_t PRECISION_VALUE = (1 << zbe::PRECISION_DIGITS);
-//
-///** \brief This constant represent half the minimal value that will keep its value after be rounded.
-// * Used to test if a value must be rounded up or down.
-// */
-//static const int64_t HALF_PRECISION_VALUE = PRECISION_VALUE/2;
-//
-///** \brief This constant represent the amount of bits that will be used as precision digits
-// * before (and after) make a division.
-// * I.E. :
-// * instead of : v1 / v2 do ((v1 << DIV_PRECISION_DIGITS)/v2) >> DIV_PRECISION_DIGITS)
-//*/
-//static const int DIV_PRECISION_DIGITS = PRECISION_DIGITS+1;
 
 /** \brief Used to compare doubles.
  */
@@ -82,6 +57,7 @@ static const double TIME_QUANTUM_VALUE = (double)TIME_QUANTUM / SECOND;
 /** \brief This constant represent the minimal amount of bits that will be used as precision.
  */
 static const int64_t ROUND_MASK = -TIME_QUANTUM;
+
 
 /** \brief This constant is used to transform the time stored in a uint64_t (nanoseconds)to the time in a velocity (in
  *  seconds, because the velocity is in m/s).
@@ -109,20 +85,21 @@ inline int64_t roundUp (double n) {
     return ((n > 0) ? ceil(n) : floor(n));
 }
 
-/*inline int64_t div(int64_t lhs, int64_t rhs){
-  int64_t result = (lhs << DIV_PRECISION_DIGITS) / rhs;
-  int64_t absResult =  llabs(result);
-  int round = absResult & 1;
-  return ((absResult+round) >> 1) * ((result>=0)*2-1);
-}
+template <unsigned dim>
+inline double sqrPointDist(Point<dim> a, Point<dim> b) {
+    double accum = 0;
+    double localDist;
+    for(unsigned i = 0; i < dim; i++) {
+        localDist = b[i] - a[i];
+        accum += localDist * localDist;
+    }
+    return accum;
+};
 
-inline int64_t mult(int64_t lhs, int64_t rhs){
-  int64_t result = lhs * rhs;
-  int64_t absResult = llabs(result);
-  int round = (absResult >> (PRECISION_DIGITS-1)) & 1;
-  absResult = (absResult >> PRECISION_DIGITS);
-  return (absResult + round) * ((result>=0)*2-1);
-}*/
+template <unsigned dim>
+inline double pointDist(Point<dim> a, Point<dim> b) {
+    return sqrt(sqrPointDist(a,b));
+};
 
 }  // namespace zbe
 
