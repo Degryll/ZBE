c6e527c4e938514216f190b6ad6d8542df51f6bc
Degryll <degryll@gmail.com>
2017-05-10 22:31:41 +0200

    issue  #77 ZBE/reactobjects

--- include/ZBE/reactobjects/VoidReactObject.h
+++ include/ZBE/reactobjects/VoidReactObject.h
@@ -2,7 +2,7 @@
  * Copyright 2012 Batis Degryll Ludo
  * @file VoidReactObject.h
  * @since 2017-01-24
- * @date 2017-01-24
+ * @date 2017-05-10
  * @author Degryll
  * @brief Simple implementations of null react objects.
  */
@@ -14,10 +14,17 @@
 
 namespace zbe {
 
+/** \brief Simple implementations of null react objects.
+ */
 template <typename R>
 class VoidReactObject : public ReactObjectCommon<VoidReactObject<R>, R> {
 public:
+  /** \brief Empty constructor.
+   */
   VoidReactObject() : zbe::ReactObjectCommon<VoidReactObject<R>, R>(this) {}
+
+  /** \brief Virtual destructor.
+   */
   virtual ~VoidReactObject() {}
 };
 
