366cd3f87509bc904319503b0f213849324667e9
RubenBatis <RubenBatis@gmail.com>
2019-03-14 22:39:11 +0100

    Issue #114 Generic Factory Added
    Added Generic Factory

---
+++ include/ZBE/factories/implementations/GenericFtry.h
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2012 Batis Degryll Ludo
+ * @file Ftry.h
+ * @since 2018-11-07
+ * @date 2018-11-21
+ * @author Ludo Degryll Batis
+ * @brief Generic Factory.
+ */
+
+#ifndef ZBE_FACTORIES_IMPLEMENTATIONS_GENERICFTRY_H_
+#define ZBE_FACTORIES_IMPLEMENTATIONS_GENERICFTRY_H_
+
+#include <string>
+
+#include "ZBE/core/tools/containers/RsrcStore.h"
+
+#include "ZBE/factories/Factory.h"
+
+#include "ZBE/core/system/system.h"
+
+namespace zbe {
+
+/** \brief Generic Factory.
+ */
+template <typename Generic, typename Specific>
+class GenericFtry : virtual public Factory {
+public:
+  /** \brief Parametrized constructor
+    * \param type Dictionary entry for this item.
+    */
+  GenericFtry(std::string gtype, std::string stype) : gtype(gtype), stype(stype) {}
+
+  /** \brief Creates an item from a file.
+   *  \param name Name for the created item.
+   *  \param cfgId item's configuration id.
+   */
+  void create(std::string name, uint64_t cfgId);
+
+  /** \brief Set-up an item.
+   *  \param name Name for the created item.
+   *  \param cfgId item's configuration id.
+   */
+  void setup(std::string name, uint64_t cfgId) {}
+
+private:
+  RsrcStore<Generic> &grsrc = RsrcStore<Generic>::getInstance();
+  RsrcStore<Specific> &srsrc = RsrcStore<Specific>::getInstance();
+  std::string gtype;
+  std::string stype;
+};
+
+template <typename Generic, typename Specific>
+void GenericFtry<Generic, Specific>::create(std::string name, uint64_t cfgId) {
+  using namespace std::string_literals;
+  auto i = std::make_shared<Specific>();
+  grsrc.insert(gtype + "."s + name, i);
+  srsrc.insert(stype + "."s + name, i);
+}
+
+} // namespace ZBE
+
+#endif // ZBE_FACTORIES_IMPLEMENTATIONS_GENERICFTRY_H_
---
+++ src/ZBETest/src/factories/implementations/GenericFtryTest.cpp
@@ -0,0 +1,65 @@
+#include "gtest/gtest.h"
+
+#include <memory>
+#include <string>
+
+#include <nlohmann/json.hpp>
+
+#include "ZBE/core/system/SysError.h"
+#include "ZBE/core/system/SysIdGenerator.h"
+
+#include "ZBE/core/tools/containers/RsrcStore.h"
+#include "ZBE/core/tools/containers/RsrcDictionary.h"
+
+#include "ZBE/core/daemons/Daemon.h"
+#include "ZBE/core/daemons/DaemonMaster.h"
+
+#include "ZBE/factories/implementations/GenericFtry.h"
+
+namespace GenericFtryTest {
+
+class DummyDaemon : public zbe::Daemon {
+public:
+  DummyDaemon(): executed(false) {}
+
+  void run(){
+    executed = true;
+  }
+  bool executed;
+};
+//-----------------------
+TEST(GenericFtryTest, build) {
+  EXPECT_EQ(0, zbe::SysError::getNErrors()) << "Initially no errors.";
+
+  using namespace zbe;
+  using namespace nlohmann;
+  using namespace std::string_literals;
+  NameRsrcDictionary &dict = NameRsrcDictionary::getInstance();
+  RsrcStore<json> &configRsrc = RsrcStore<json>::getInstance();
+  RsrcStore<zbe::Daemon> &daemonRsrc = RsrcStore<zbe::Daemon>::getInstance();
+  RsrcStore<DummyDaemon> &ddaemonRsrc = RsrcStore<DummyDaemon>::getInstance();
+
+  GenericFtry<zbe::Daemon, DummyDaemon> gf("Daemon", "DummyDaemon");
+  gf.create("GenericFtryTestName", 0);
+  gf.setup("GenericFtryTestName", 0);
+
+  std::shared_ptr<zbe::Daemon> outDmn = daemonRsrc.get("Daemon.GenericFtryTestName");
+  std::shared_ptr<DummyDaemon> outDDmn = ddaemonRsrc.get("DummyDaemon.GenericFtryTestName");
+
+  EXPECT_EQ(0, zbe::SysError::getNErrors()) << "Must create a daemon with given name";
+
+  EXPECT_FALSE(outDDmn->executed) << "Initially outDDmn not called";
+
+  outDmn->run();
+
+  EXPECT_TRUE(outDDmn->executed) << "Must call outDDmn";
+
+  configRsrc.clear();
+  dict.clear();
+  daemonRsrc.clear();
+  ddaemonRsrc.clear();
+
+  zbe::SysError::clear();
+}
+
+}  // namespace GenericFtryTest
