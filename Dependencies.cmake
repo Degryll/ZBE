7f3ec38a42f4eb54750a48442624a614d917ef45
Israel Bautista <isra.bautista@gmail.com>
2025-02-13 22:23:36 +0100

    Issue #142 Set proper project name.

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 # Done as a function so that updates to variables like
 # CMAKE_CXX_FLAGS don't propagate out to other
 # targets
-function(myproject_setup_dependencies)
+function(ZBE_setup_dependencies)
 
   # For each dependency, see if it's
   # already been provided to us by a parent project
