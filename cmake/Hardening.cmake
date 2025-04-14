f820cf342c82db9a289ddd2e9e222d06a8597162
Israel Bautista <isra.bautista@gmail.com>
2025-04-14 23:14:30 +0200

    Test Clang config.

--- cmake/Hardening.cmake
+++ cmake/Hardening.cmake
@@ -97,6 +97,6 @@ macro(
   endif()
 
   if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libstdc++")
   endif()
 endmacro()
